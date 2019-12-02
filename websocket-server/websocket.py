4502de69ee04b2dcb56099231855fa59fd165532
muramasa <muramasa125@gmail.com>
2019-12-03 00:09:43 +0900

    websocket connection got

--- websocket-server/websocket.py
+++ websocket-server/websocket.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 import io
+import os
 
 class WebSockets_Server:
 
@@ -15,15 +16,19 @@ class WebSockets_Server:
           recv_data = await websocket.recv()
           send_message = text + ':' + recv_data
           await websocket.send(send_message)
-          print(send_message)
+          await websocket.send('server-path:' + get_full_path())
+          print('message send!!')
 
     def run(self):
         self._server = websockets.serve(self._handler, self.address, self.port)
         print('websocket-server start...')
         self.loop.run_until_complete(self._server)
         self.loop.run_forever()
+    
+def get_full_path():
+    return os.getcwd()+ '/' + __file__
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    wss = WebSockets_Server(loop, '127.0.0.1', 60000, 'websocket-server通信成功!!')
+    wss = WebSockets_Server(loop, '127.0.0.1', 60000, 'websocket-server is recieved -> ')
     wss.run()
